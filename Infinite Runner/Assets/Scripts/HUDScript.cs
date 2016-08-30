21fc91de8f52e23c0e453aafa7572f72f7817744
Salvador Cantu <secantu@fullsail.edu>
2016-08-30 15:55:36 -0400

    added Infinte Runner to the Repo

---
+++ Infinite Runner/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+    float playerScore = 0;
+    public Text scoreText;
+	
+	void Update () {
+        playerScore += Time.deltaTime;
+        scoreText.text = "Score: " + (int)(playerScore * 100);
+
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        playerScore += amount;
+    }
+    
+    void OnDisable()
+    {
+        PlayerPrefs.SetInt("Score", (int)(playerScore * 100));
+    }
+}
---
+++ Infinite Runner/Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpScript : MonoBehaviour {
+
+    HUDScript HUD;
+    GameObject camera;
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            HUD = camera.GetComponent<HUDScript>();
+            HUD.IncreaseScore(10);
+            Destroy(this.gameObject);
+        }
+    }
+}
