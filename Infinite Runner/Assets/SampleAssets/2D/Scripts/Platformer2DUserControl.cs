7c1479c9aff08bb4192482370927f0f8a2b87448
Salvador Cantu <secantu@fullsail.edu>
2016-09-03 02:17:04 -0400

    Changed Main character. added animations (still need to add animations for jump shooting and death as well as implement the animations.

--- Infinite Runner/Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
+++ Infinite Runner/Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
@@ -3,32 +3,35 @@ using UnitySampleAssets.CrossPlatformInput;
 
 namespace UnitySampleAssets._2D
 {
-
-    [RequireComponent(typeof (PlatformerCharacter2D))]
+    [RequireComponent(typeof(PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
         private PlatformerCharacter2D character;
         private bool jump;
-
+        private float runDirection;
+        private Animator anim; // Reference to the player's animator component.
+    
         private void Awake()
         {
             character = GetComponent<PlatformerCharacter2D>();
+            anim = GetComponent<Animator>();
         }
-
         private void Update()
         {
-            if(!jump)
-            // Read the jump input in Update so button presses aren't missed.
-            jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            if (!jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
         }
-
         private void FixedUpdate()
         {
             // Read the inputs.
             //bool crouch = Input.GetKey(KeyCode.LeftControl);
             //float h = CrossPlatformInputManager.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
-            character.Move(1, false, jump);
+            runDirection = Input.GetAxis("Horizontal");
+            character.Move(runDirection, jump);
             jump = false;
         }
     }
---
+++ Infinite Runner/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+
+    private bool jump;
+    [SerializeField]
+    float speed = 3f;
+
+    [SerializeField]
+    private float jumpForce = 400f;
+
+     Animator anim;
+
+    bool doubleJump = false;
+    bool grounded = false;
+
+    [SerializeField]
+    private bool airControl = false; // Whether or not a player can steer while jumping;
+    [SerializeField]
+    private LayerMask whatIsGround; // A mask determining what is ground to the character
+    private Transform groundCheck; // A position marking where to check if the player is grounded.
+    private float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    
+
+    // Use this for initialization
+    void Start () {
+        groundCheck = transform.Find("GroundCheck");
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!jump)
+            jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        
+
+    }
+
+    void fixedUpdate()
+    {
+       // var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+       // transform.position += move * speed * Time.deltaTime;
+         Move(Input.GetAxis("Horizontal"), jump);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+        if (grounded)
+        {
+            jump = false;
+            doubleJump = false;
+        }
+    }
+
+    public void Move(float move, bool jump)
+    {
+        //only control the player if grounded or airControl is turned on
+        if (grounded || airControl)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        // If the player should jump...
+        if ((grounded || !doubleJump) && jump /*&& anim.GetBool("Ground")*/)
+        {
+            // grounded = false;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0f);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+            if (!grounded)
+                doubleJump = true;
+        }
+    }
+}
